43241bfc7fff3ab764b15722d7dba7e5b27e43d6
Kevin <kevin.jablonka@epfl.ch>
2020-09-28 15:49:53 +0200

    chore: added tests

--- tests/test_models_gpy.py
+++ tests/test_models_gpy.py
@@ -3,7 +3,12 @@
 import GPy
 
 from PyPAL.models.coregionalized import GPCoregionalizedRegression
-from PyPAL.models.gpr import build_coregionalized_model, build_model
+from PyPAL.models.gpr import (
+    build_coregionalized_model,
+    build_model,
+    predict,
+    predict_coregionalized,
+)
 
 
 def test_build_model(make_random_dataset):
@@ -20,3 +25,34 @@ def test_build_coregionalized_model(make_random_dataset):
     X, y = make_random_dataset  # pylint:disable=invalid-name
     m = build_coregionalized_model(X, y)  # pylint:disable=invalid-name
     assert isinstance(m, GPCoregionalizedRegression)
+
+
+def test_predict(make_random_dataset):
+    """Test that the prediction function works"""
+    X, y = make_random_dataset  # pylint:disable=invalid-name
+    m = build_model(X, y)  # pylint:disable=invalid-name
+    prediction = predict(m, X)
+    assert len(prediction) == 2
+    assert len(prediction[0]) == 100
+    assert len(prediction[1]) == 100
+    assert prediction[0].shape[1] == 1
+    assert prediction[1].shape[1] == 1
+
+
+def test_predict_coregionalized(make_random_dataset):
+    """"Test that the prediction utility for coregionalized models works"""
+    X, y = make_random_dataset  # pylint:disable=invalid-name
+    m = build_coregionalized_model(X, y)  # pylint:disable=invalid-name
+    prediction = predict_coregionalized(m, X)
+    assert len(prediction) == 2
+    assert len(prediction[0]) == 100
+    assert len(prediction[1]) == 100
+    assert prediction[0].shape[1] == 1
+    assert prediction[1].shape[1] == 1
+
+    prediction2 = predict_coregionalized(m, X, 1)
+    assert len(prediction2) == 2
+    assert len(prediction2[0]) == 100
+    assert len(prediction2[1]) == 100
+    assert prediction2[0].shape[1] == 1
+    assert prediction2[1].shape[1] == 1
