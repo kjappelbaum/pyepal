e58b93f529c9ede817f43c5e0682c6bc4e8710f4
Kevin <kevin.jablonka@epfl.ch>
2020-09-30 12:38:32 +0200

    feat: renaming package, addressing #5

--- PyPAL/__init__.py
+++ PyPAL/__init__.py
@@ -10,3 +10,7 @@ from .pal.pal_base import PALBase
 from .pal.pal_coregionalized import PALCoregionalized
 from .pal.pal_gpy import PALGPy
 from .pal.pal_sklearn import PALSklearn
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
