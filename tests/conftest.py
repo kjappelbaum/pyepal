c49ba8c5ea77210ab6b02e590716293f4792b96d
Kevin <kevin.jablonka@epfl.ch>
2020-10-01 23:03:53 +0200

    chore: added bihn korn test function as fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 """Setting up pytest"""
-from __future__ import absolute_import
-
+import numpy as np
 import pytest
 from sklearn.datasets import make_regression
+
+
+def binh_korn(x, y):  # pylint:disable=invalid-name
+    """https://en.wikipedia.org/wiki/Test_functions_for_optimization"""
+    obj1 = 4 * x ** 2 + 4 * y ** 2
+    obj2 = (x - 5) ** 2 + (y - 5) ** 2
+    return obj1, obj2
+
+
+@pytest.fixture()
+def binh_korn_points():
+    """Create a dataset based on the Binh-Korn test function"""
+    x = np.linspace(0, 5, 100)  # pylint:disable=invalid-name
+    y = np.linspace(0, 3, 100)  # pylint:disable=invalid-name
+    array = np.array([binh_korn(xi, yi) for xi, yi in zip(x, y)])
+    return array
 
 
 @pytest.fixture()
