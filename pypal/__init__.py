e526201b23519047bb874b32e7ac8a07dec998b4
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-10-17 12:34:01 +0200

    chore: updating docs #29

--- pypal/__init__.py
+++ pypal/__init__.py
@@ -5,7 +5,12 @@ from .pal.pal_base import PALBase
 from .pal.pal_coregionalized import PALCoregionalized
 from .pal.pal_gpy import PALGPy
 from .pal.pal_sklearn import PALSklearn
-from .pal.utils import get_hypervolume, get_kmeans_samples, get_maxmin_samples
+from .pal.utils import (
+    exhaust_loop,
+    get_hypervolume,
+    get_kmeans_samples,
+    get_maxmin_samples,
+)
 
 __version__ = get_versions()["version"]
 del get_versions
