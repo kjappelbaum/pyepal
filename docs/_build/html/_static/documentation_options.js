056e023b7b05a4d4ae7d02961f632ed96f977985
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-10-18 15:58:59 +0200

    docs: fixed typo

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.158.ge637aa3.dirty',
+    VERSION: '0+untagged.160.gcbda9d8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
