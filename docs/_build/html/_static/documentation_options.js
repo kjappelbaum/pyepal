0e0419dfd8ae06455cb8592942d861d0e87c02a4
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-10-18 14:30:23 +0200

    docs: adding beta influence

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.149.g806fb80.dirty',
+    VERSION: '0+untagged.150.g8d8072f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
