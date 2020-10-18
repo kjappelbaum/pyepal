f60edd1a259b41fb18ad4d2988d4d6a5c9476616
Kevin Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2020-10-18 15:37:23 +0200

    docs: added docstring to the PAL classes #40 (#43)

--- pypal/pal/pal_sklearn.py
+++ pypal/pal/pal_sklearn.py
@@ -22,6 +22,26 @@ class PALSklearn(PALBase):
     """PAL class for a list of Sklearn (GPR) models, with one model per objective"""
 
     def __init__(self, *args, **kwargs):
+        """Construct the PALSklearn instance
+
+        Args:
+            X_design (np.array): Design space (feature matrix)
+            models (list): Machine learning models
+            ndim (int): Number of objectives
+            epsilon (Union[list, float], optional): Epsilon hyperparameter.
+                Defaults to 0.01.
+            delta (float, optional): Delta hyperparameter. Defaults to 0.05.
+            beta_scale (float, optional): Scaling parameter for beta.
+                If not equal to 1, the theoretical guarantees do not necessarily hold.
+                Also note that the parametrization depends on the kernel type.
+                Defaults to 1/9.
+            goals (List[str], optional): If a list, provide "min" for every objective
+                that shall be minimized and "max" for every objective
+                that shall be maximized. Defaults to None, which means
+                that the code maximizes all objectives.
+            n_jobs (int): Number of parallel processes that are used to fit
+                the GPR models. Defaults to 1.
+        """
         n_jobs = kwargs.pop("n_jobs", 1)
         validate_njobs(n_jobs)
         self.n_jobs = n_jobs
