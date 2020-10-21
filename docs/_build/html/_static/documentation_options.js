fadd2b97f064a0062dfdc7105bc291b91eaa181d
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-10-21 14:23:43 +0200

    docs: rebuild docs

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.201.g1699f5c.dirty',
+    VERSION: '0+untagged.199.ge60f073',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
