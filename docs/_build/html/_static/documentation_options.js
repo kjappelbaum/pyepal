c6961ab20924bf2b452339793f8e2266e5a49905
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-10-21 08:31:47 +0200

    docs: added note about coef_var_threshold, closes #71

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.193.gcb2a315.dirty',
+    VERSION: '0+untagged.195.g4bae94d.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
