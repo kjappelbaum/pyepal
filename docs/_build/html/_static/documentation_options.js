d9e17ac2a7e38f8bd9b5c24b0b756e00f1deddce
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-10-25 14:53:40 +0100

    docs: added some links to API docs, explain which class to use. Closes #78

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.210.gc6bd515.dirty',
+    VERSION: '0+untagged.212.g4af8189.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
