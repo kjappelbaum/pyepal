4806c4493939abbe79fa7a9a1045e46772a42e88
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2020-11-23 14:02:30 +0100

    feat: basic, untested implementation of the infinite width inference

--- pyepal/pal/pal_neural_tangent.py
+++ pyepal/pal/pal_neural_tangent.py
@@ -39,9 +39,9 @@ from ..models.nt_stax import NT_TUPLE
 # Probably we also need to add jaxlib to the dependencies
 from .pal_base import PALBase
 
-__all__ = ["PALNT"]
+__all__ = ["PALNT", "NT_TUPLE"]
 
-
+# We move those functions out of the class so that we can parallelize them
 def _set_one_infinite_width_model(
     i: int,
     models: Sequence[NT_TUPLE],
@@ -76,10 +76,55 @@ class PALNT(PALBase):
     (in the current implementation, one model per objective)"""
 
     def __init__(self, *args, **kwargs):
+        """Construct the PALNT instance
+
+        Args:
+            X_design (np.array): Design space (feature matrix)
+            models (Sequence[NT_TUPLE]): You need to provide a sequence of
+                namedtuples NT_TUPLE (`pyepal.models.nt_stax.NT_TUPLE`).
+                The elements of this tuple are the `apply_fn`, `init_fn`,
+                `kernel_fn` and `predict_fn` (for latter you can typically
+                provide `None`)
+            ndim (int): Number of objectives
+            epsilon (Union[list, float], optional): Epsilon hyperparameter.
+                Defaults to 0.01.
+            delta (float, optional): Delta hyperparameter. Defaults to 0.05.
+            beta_scale (float, optional): Scaling parameter for beta.
+                If not equal to 1, the theoretical guarantees do not necessarily hold.
+                Also note that the parametrization depends on the kernel type.
+                Defaults to 1/9.
+            goals (List[str], optional): If a list, provide "min" for every objective
+                that shall be minimized and "max" for every objective
+                that shall be maximized. Defaults to None, which means
+                that the code maximizes all objectives.
+            coef_var_threshold (float, optional): Use only points with
+                a coefficient of variation below this threshold
+                in the classification step. Defaults to 3.
+            kernel (str, optional): The kernel type you want to use ('nngp' or 'ntk').
+                Defaults to 'nngp'.
+        """
+        self.kernel = kwargs.pop("kernel", "nngp")
+
         super().__init__(*args, **kwargs)
 
+    def _set_data(self):
+        for i in range(len(self.models)):
+            predict_fn = _set_one_infinite_width_model(
+                i, self.models, self.design_space, self.y, self.sampled
+            )
+            self.models[i].predict_fn = predict_fn
+
     def _train(self):
-        ...
+        pass
 
     def _predict(self):
-        ...
+        means, stds = [], []
+        for i in range(len(self.models)):
+            mean, std = _predict_one_infinite_width_model(
+                i, self.models, self.design_space, self.kernel
+            )
+            means.append(mean)
+            stds.append(std)
+
+        self.means = np.hstack(means)
+        self.std = np.hstack(stds)
