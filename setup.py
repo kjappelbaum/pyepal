5e7651dc35b60f4725b3dcf728ae448e6537622f
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-09-24 21:13:55 +0200

    fix: update dependencies for nt

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ gpy_requirements = [
     "matplotlib==3.*",
 ]
 gbdt_requirements = ["lightgbm==3.*"]
-neural_tangents_requirements = ["neural_tangents==0.*"]
+neural_tangents_requirements = ["neural_tangents==0.*", "jax", "jaxlib"]
 gpflow_requirements = ["gpflow"]
 setup(
     name="pyepal",
